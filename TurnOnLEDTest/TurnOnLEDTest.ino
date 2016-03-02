c400620fc90f6f2e28e2a6fa298a55a015aa8351
danielkenwahlau <danielkenwahlau@gmail.com>
2016-03-02 14:22:35 -0500

    Changing to Blink the LEDs one at a time

--- TurnOnLEDTest/TurnOnLEDTest.ino
+++ TurnOnLEDTest/TurnOnLEDTest.ino
@@ -4,16 +4,23 @@ void setup()
   pinMode(11, OUTPUT); //GREEN
   pinMode(12, OUTPUT); //YELLOW
   pinMode(13, OUTPUT); //RED
-  digitalWrite(11, HIGH);
-  digitalWrite(12, HIGH);
-  digitalWrite(13, HIGH);
   analogReference(DEFAULT);
 }
 
 void loop() {
 
+
   //test to just turn on all the lights
     digitalWrite(11, HIGH);
+    delay(1000);
+    digitalWrite(11, LOW);
+    
     digitalWrite(12, HIGH);
+    delay(1000);
+    digitalWrite(12, LOW);
+    
     digitalWrite(13, HIGH);
+    delay(1000);
+    digitalWrite(13, LOW);
+    
 }
